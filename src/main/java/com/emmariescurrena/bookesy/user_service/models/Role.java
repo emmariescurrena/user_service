9b6ba8d487844cecdbc513890a69ca99b06e1f1a
emmariescurrena <emmariescurrena>
2024-09-27 19:26:48 -0300

    Defines User and Role models

---
+++ src/main/java/com/emmariescurrena/bookesy/user_service/models/Role.java
@@ -0,0 +1,26 @@
+package com.emmariescurrena.bookesy.user_service.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "ROLES")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private RoleEnum name;
+
+}

219ce3174ddea97a6c2ca662b913fce2eab2240d
emmariescurrena <emmariescurrenadev@gmail.com>
2024-09-29 09:55:49 -0300

    Adds proper annotations

--- src/main/java/com/emmariescurrena/bookesy/user_service/models/Recommendation.java
+++ src/main/java/com/emmariescurrena/bookesy/user_service/models/Recommendation.java
@@ -2,12 +2,16 @@ package com.emmariescurrena.bookesy.user_service.models;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "RECOMMENDATIONS")
 public class Recommendation {
 
     @Id

fd9cec9d71eb97757d253ce549e9f8d00098b3d5
emmariescurrena <emmariescurrenadev@gmail.com>
2024-10-08 09:36:10 -0300

    Replaces createUser's user parameter by userDto and simplifies getUser

--- src/main/java/com/emmariescurrena/bookesy/user_service/controllers/UserController.java
+++ src/main/java/com/emmariescurrena/bookesy/user_service/controllers/UserController.java
@@ -1,18 +1,21 @@
 package com.emmariescurrena.bookesy.user_service.controllers;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.emmariescurrena.bookesy.user_service.dtos.UserDto;
+import com.emmariescurrena.bookesy.user_service.dtos.UserRequestDto;
 import com.emmariescurrena.bookesy.user_service.models.User;
 import com.emmariescurrena.bookesy.user_service.services.UserService;
+
+import jakarta.validation.Valid;
 
 
 @RestController
@@ -23,25 +26,21 @@ public class UserController {
     UserService userService;
 
     @PostMapping
-    public ResponseEntity<?> createUser(User user) {
-        User createdUser = userService.createUser(user);
-        return ResponseEntity.ok(createdUser);
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@Valid @RequestBody UserRequestDto userDto) {
+        return userService.createUser(new User(userDto));
     }
 
     @GetMapping("/byId/{id}")
-    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
-        return Optional
-            .ofNullable(userService.getUserById(id))
-            .map( user -> ResponseEntity.ok(new UserDto(user.get())) )          //200 OK
-            .orElseGet( () -> ResponseEntity.notFound().build() );
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return ResponseEntity.of(userService.getUserById(id));
     }
 
     @GetMapping("/byEmail/{email}")
-    public ResponseEntity<UserDto> getUserByEmail(@PathVariable String email) {
-        return Optional
-            .ofNullable(userService.getUserByEmail(email))
-            .map( user -> ResponseEntity.ok(new UserDto(user.get())) )          //200 OK
-            .orElseGet( () -> ResponseEntity.notFound().build() );
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
+        return ResponseEntity.of(userService.getUserByEmail(email));
     }
 
 }

43ab7e6606dbb4bea0adcbe2cd5e903bc2afedf0
emmariescurrena <emmariescurrenadev@gmail.com>
2024-10-17 11:14:33 -0300

    Adds validation to CreateUserDto

--- src/main/java/com/emmariescurrena/bookesy/user_service/dtos/CreateUserDto.java
+++ src/main/java/com/emmariescurrena/bookesy/user_service/dtos/CreateUserDto.java
@@ -1,14 +1,33 @@
 package com.emmariescurrena.bookesy.user_service.dtos;
 
+import com.emmariescurrena.bookesy.user_service.util.RegexValidator;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreateUserDto {
 
     private String auth0UserId;
+    
+    @NotEmpty(message = "The email is required")
+    @Email(regexp = RegexValidator.EMAIL,
+            flags = Pattern.Flag.CASE_INSENSITIVE,
+            message = "Invalid email format")
     private String email;
+
+    @NotEmpty(message = "The name is required")
+    @Size(min = 2, max = 100, message = "The length of name must be between 2 and 100 characters")
     private String name;
+
+    @NotEmpty(message = "The surname is required")
+    @Size(min = 2, max = 100, message = "The length of surname must be between 2 and 100 characters")
     private String surname;
+
+    @Size(max = 1000, message = "The maximum length of bio is 1000 characters")
     private String bio;
 
 }

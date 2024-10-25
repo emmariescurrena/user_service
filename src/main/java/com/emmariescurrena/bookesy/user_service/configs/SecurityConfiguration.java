53c26e1baf6ae65a446a146a7bb93e6dfec01afe
emmariescurrena <emmariescurrenadev@gmail.com>
2024-10-25 18:57:03 -0300

    Creates SecurityConfiguration

---
+++ src/main/java/com/emmariescurrena/bookesy/user_service/configs/SecurityConfiguration.java
@@ -0,0 +1,47 @@
+package com.emmariescurrena.bookesy.user_service.configs;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf((csrf) -> csrf.disable())
+            .cors((cors) -> cors.configurationSource(corsConfigurationSource()))
+            .authorizeHttpRequests(authz -> authz
+                .requestMatchers(HttpMethod.POST, "/users/**").permitAll()
+                .anyRequest().authenticated()
+            );
+        return http.build();
+
+    }
+    
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}

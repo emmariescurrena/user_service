547edbc0e77d6753ec64fc8a8d5a3273f3afa792
emmariescurrena <emmariescurrenadev@gmail.com>
2024-10-27 19:22:08 -0300

    Removes username

--- src/main/java/com/emmariescurrena/bookesy/user_service/dtos/CreateUserDto.java
+++ src/main/java/com/emmariescurrena/bookesy/user_service/dtos/CreateUserDto.java
@@ -5,7 +5,6 @@ import com.emmariescurrena.bookesy.user_service.util.RegexValidator;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -15,11 +14,6 @@ public class CreateUserDto {
     @Email(regexp = RegexValidator.EMAIL,
             flags = Pattern.Flag.CASE_INSENSITIVE,
             message = "Invalid email format")
-    private String email;
-
-    @NotEmpty(message = "The username is required")
-    @Size(min = 2, max = 15, message = "The length of name must be between 2 and 15 characters")
-    private String username;
-
+    private String email;   
 
 }

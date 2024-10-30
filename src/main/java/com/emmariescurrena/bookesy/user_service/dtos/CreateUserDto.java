71de8924ad72a6c152d76675cab37b921f2d0aeb
emmariescurrena <emmariescurrena>
2024-10-30 12:25:33 -0300

    Adds name and surname

--- src/main/java/com/emmariescurrena/bookesy/user_service/dtos/CreateUserDto.java
+++ src/main/java/com/emmariescurrena/bookesy/user_service/dtos/CreateUserDto.java
@@ -5,6 +5,7 @@ import com.emmariescurrena.bookesy.user_service.util.RegexValidator;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -14,6 +15,12 @@ public class CreateUserDto {
     @Email(regexp = RegexValidator.EMAIL,
             flags = Pattern.Flag.CASE_INSENSITIVE,
             message = "Invalid email format")
-    private String email;   
+    private String email;
+
+    @Size(min = 2, max = 100, message = "The length of name must be between 2 and 100 characters")
+    private String name;
+
+    @Size(min = 2, max = 100, message = "The length of surname must be between 2 and 100 characters")
+    private String surname;
 
 }

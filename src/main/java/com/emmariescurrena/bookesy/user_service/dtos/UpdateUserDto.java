6c3cf6eefeada9e48b99b17c3ef0463367ed55b5
emmariescurrena <emmariescurrena>
2024-10-30 12:23:42 -0300

    Removes email

--- src/main/java/com/emmariescurrena/bookesy/user_service/dtos/UpdateUserDto.java
+++ src/main/java/com/emmariescurrena/bookesy/user_service/dtos/UpdateUserDto.java
@@ -1,20 +1,10 @@
 package com.emmariescurrena.bookesy.user_service.dtos;
 
-import com.emmariescurrena.bookesy.user_service.util.RegexValidator;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UpdateUserDto {
-
-    @Size(max = 320, message = "The max length of email must be 320 characters")
-    @Email(regexp = RegexValidator.EMAIL,
-            flags = Pattern.Flag.CASE_INSENSITIVE,
-            message = "Invalid email format")
-    private String email;
 
     @Size(min = 2, max = 100, message = "The length of name must be between 2 and 100 characters")
     private String name;

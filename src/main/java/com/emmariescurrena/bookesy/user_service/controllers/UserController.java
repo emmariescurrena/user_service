a8d0e757adb25c9424247a151490c64674157c65
emmariescurrena <emmariescurrenadev@gmail.com>
2024-10-30 12:22:47 -0300

    Removes getAuthentication

--- src/main/java/com/emmariescurrena/bookesy/user_service/controllers/UserController.java
+++ src/main/java/com/emmariescurrena/bookesy/user_service/controllers/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -79,14 +78,6 @@ public class UserController {
         User userToDelete = getUserFromOptional(optionalUserToDelete);
         return deleteUser(userToDelete);
     }
-
-    @GetMapping("/getAuthentication")
-    public void getAuthentication(@AuthenticationPrincipal Object authPrinc, Authentication authentication, @AuthenticationPrincipal(errorOnInvalidType=true) UserDetails userDetails) {
-        log.info(authPrinc.toString());
-        log.info(authentication.toString());
-        log.info(userDetails.toString());
-    }
-    
 
     private User updateUser(User userToUpdate, UpdateUserDto userDto) {
         return userService.updateUser(userToUpdate, userDto);

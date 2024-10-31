1d46453977169f3a9dcd509c6057df3f2f063315
emmariescurrena <emmariescurrenadev@gmail.com>
2024-10-31 19:38:07 -0300

    Creates UserInfo model

---
+++ src/main/java/com/emmariescurrena/bookesy/user_service/models/UserInfo.java
@@ -0,0 +1,37 @@
+package com.emmariescurrena.bookesy.user_service.models;
+
+import java.time.ZonedDateTime;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+public class UserInfo {
+    @JsonProperty("sub")
+    private String sub;
+
+    @JsonProperty("given_name")
+    private String givenName;
+
+    @JsonProperty("family_name")
+    private String familyName;
+
+    @JsonProperty("nickname")
+    private String nickname;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("picture")
+    private String picture;
+
+    @JsonProperty("updated_at")
+    private ZonedDateTime updatedAt;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("email_verified")
+    private boolean emailVerified;
+}

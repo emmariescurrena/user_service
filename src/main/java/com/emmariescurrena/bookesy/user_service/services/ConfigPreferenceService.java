08bcdefe7839af6d30a9b57f9e93c52a771d9e54
emmariescurrena <emmariescurrena>
2024-11-07 19:42:17 -0300

    Creates ConfigPreferenceService

---
+++ src/main/java/com/emmariescurrena/bookesy/user_service/services/ConfigPreferenceService.java
@@ -0,0 +1,43 @@
+package com.emmariescurrena.bookesy.user_service.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.emmariescurrena.bookesy.user_service.models.ConfigPreference;
+import com.emmariescurrena.bookesy.user_service.models.ConfigPreferenceEnum;
+import com.emmariescurrena.bookesy.user_service.repositories.ConfigPreferenceRepository;
+
+@Service
+public class ConfigPreferenceService {
+    
+    @Autowired
+    ConfigPreferenceRepository configPreferenceRepository;
+
+    @Autowired
+    UserService userService;
+    
+    public List<ConfigPreference> getConfigPreferences(Long userId) {
+        return configPreferenceRepository.findByUserId(userId);
+    }
+
+    @Transactional
+    public void upsertConfigPreference(Long userId, ConfigPreferenceEnum preferenceName, String value) {
+        ConfigPreference configPreference = configPreferenceRepository
+                .findByUserIdAndName(userId, preferenceName)
+                .orElse(null);
+                
+        if (configPreference == null) {
+            configPreference = new ConfigPreference();
+            configPreference.setUser(userService.getUserById(userId).get());
+            configPreference.setName(preferenceName);
+        }
+
+        configPreference.setValue(value);
+        configPreferenceRepository.save(configPreference);
+    }
+
+
+}

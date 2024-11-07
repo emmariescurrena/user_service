bd5e49da041e972d5fc36acf4d2dd429a0997607
emmariescurrena <emmariescurrena>
2024-11-07 10:59:12 -0300

    Adds user field and removes unique from name

--- src/main/java/com/emmariescurrena/bookesy/user_service/models/ConfigPreference.java
+++ src/main/java/com/emmariescurrena/bookesy/user_service/models/ConfigPreference.java
@@ -4,9 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -19,11 +22,15 @@ public class ConfigPreference {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private ConfigPreferencesEnum name;
 
     @Column(nullable = false)
     private String value;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
     
 }

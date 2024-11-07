31b464c22743168c47b57e1f0d980375cf3a311c
emmariescurrena <emmariescurrena>
2024-11-07 19:29:13 -0300

    Removes findByName and adds findByUserId and findByUserIdAndName

--- src/main/java/com/emmariescurrena/bookesy/user_service/repositories/ConfigPreferenceRepository.java
+++ src/main/java/com/emmariescurrena/bookesy/user_service/repositories/ConfigPreferenceRepository.java
@@ -1,15 +1,17 @@
 package com.emmariescurrena.bookesy.user_service.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.emmariescurrena.bookesy.user_service.models.ConfigPreference;
-import com.emmariescurrena.bookesy.user_service.models.ConfigPreferencesEnum;
+import com.emmariescurrena.bookesy.user_service.models.ConfigPreferenceEnum;
 
 @Repository
 public interface ConfigPreferenceRepository extends JpaRepository<ConfigPreference, Long> {
-    Optional<ConfigPreference> findByName(ConfigPreferencesEnum name);
+    List<ConfigPreference> findByUserId(Long userId);
+    Optional<ConfigPreference> findByUserIdAndName(Long userId, ConfigPreferenceEnum name);
 }
 

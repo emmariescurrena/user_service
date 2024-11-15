9fe3112450754cac93a892cfe3f47d7cc3a02813
emmariescurrena <emmariescurrena>
2024-11-15 08:02:47 -0300

    Removes NotNull annotation

--- src/main/java/com/emmariescurrena/bookesy/user_service/dtos/UpsertConfigPreferenceDto.java
+++ src/main/java/com/emmariescurrena/bookesy/user_service/dtos/UpsertConfigPreferenceDto.java
@@ -1,21 +1,17 @@
 package com.emmariescurrena.bookesy.user_service.dtos;
 
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class UpsertConfigPreferenceDto {
 
-    @NotNull
     @Pattern(regexp = "enabled|disabled", message = "Notification must be either 'enabled' or 'disabled'")
     private String notification;
 
-    @NotNull
     @Pattern(regexp = "dark|light", message = "Theme must be either 'dark' or 'light'")
     private String theme;
 
-    @NotNull
     @Pattern(regexp = "en|es", message = "Language must be either 'en' or 'es'")
     private String language;
 

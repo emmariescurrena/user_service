996881522ada7947bfeb11a5989667664ed7b519
emmariescurrena <emmariescurrena>
2024-11-25 08:31:29 -0300

    Upgrades Spring Boot to 3.3.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.3.5"
+	id("org.springframework.boot") version "3.3.6"
 	id("io.spring.dependency-management") version "1.1.6"
     id("io.freefair.lombok") version "8.10"
 }

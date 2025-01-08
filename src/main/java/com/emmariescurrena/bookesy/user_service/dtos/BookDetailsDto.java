3aaa4644d1b4c3f71070323766ecde566771bf1a
emmariescurrena <emmariescurrenadev@gmail.com>
2025-01-08 13:08:56 -0300

    Adds averageRating and ratingCount and changes publishedYear class to Integer

--- src/main/java/com/emmariescurrena/bookesy/user_service/dtos/BookDetailsDto.java
+++ src/main/java/com/emmariescurrena/bookesy/user_service/dtos/BookDetailsDto.java
@@ -1,6 +1,5 @@
 package com.emmariescurrena.bookesy.user_service.dtos;
 
-import java.time.Year;
 import java.util.List;
 
 import com.emmariescurrena.bookesy.user_service.models.Author;
@@ -14,9 +13,11 @@ public class BookDetailsDto {
     private String id;
     private String title;
     private String description;
-    private Year publishedYear;
+    private Integer publishedYear;
     private Integer coverId;
     private List<Author> authors;
     private List<Genre> genres;
+    private Double averageRating;
+    private Integer ratingCount;
 
 }
